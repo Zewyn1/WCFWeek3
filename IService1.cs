23d649a9489b31cd7831f1af2a277a23b967534e
Zewyn1 <danail.atanasov33@gmail.com>
2022-04-26 13:01:42 +0300

    Initial commit

---
+++ IService1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFDani
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+        [OperationContract]
+        string GetDays(int day, int month, int year);
+        [OperationContract]
+        string GetNames(string fname, string lname, string bday);
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+    }
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFDani
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        public string GetDays(int day,int month,int year)
+        {
+            ClassLibDani1.SampleFunctions service=new ClassLibDani1.SampleFunctions();
+            string days = service.CalculateDays(day, month, year);
+            return days;
+            //DateTime dt = new DateTime(year, month, day);
+            //int daysSince = DateTime.Now.Subtract(dt).Days;
+            //return daysSince.ToString();
+        }
+        public string GetNames(string fname, string lname,string bday)
+        {
+            ClassLibDani1.SampleFunctions service = new ClassLibDani1.SampleFunctions();
+            string fullnames=service.GetFullNames(fname, lname, bday);
+            return fullnames;
+        }
+    }
+}
